b413a2d00b9fcb00261882133efea4b64b111f93
Abdullah Ayman <abdulluhayman@gmail.com>
2025-12-22 04:27:07 +0200

    Initalized the packages ,including linter, jest, and tailwind, react for the webpage

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
+    },
+    transform: {
+        "^.+\\.(js|jsx)$": "babel-jest",
+    }
+};
